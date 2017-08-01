6ef14f94a62363229dcac62c0978fa45ba9a93ba
Koby Picker <jkp46@case.edu>
2017-08-01 18:50:49 +0000

    renders file content with proper encoding, removed default display, modified layout
    Signed-off-by: Koby Picker <jkp46@case.edu>

--- src/App.js
+++ src/App.js
@@ -3,97 +3,99 @@ import logo from './logo.svg';
 import './App.css';
 import {Head, Nav, Display} from './components.js';
 
-
+function newPR() {return {id: null, clones: [], files: []}}
 
 class App extends Component {
-    constructor() {
-        super();
-        this.state = {repo: null, user:null, prs: [], activePR:{id: null, clones: []}};
-        let url = window.location.href;
-        // determine if url is http(s)://something/something/something
-        if (!url.match('https?://[^/]+/pr/[^/]+/[^/]+$')) {
-            console.log("badly formed url:");
-            console.log(url);
-            return;
-        }
-        if (url.lastIndexOf("/") === url.length -1) {
-            url = url.substring(0,-1);
-        }
-        let sep = url.lastIndexOf("/");
-        let repo = url.substring(sep + 1);
-        let user = url.substring(url.substring(0,sep).lastIndexOf("/")+1, sep);
-        this.getPRs = this.getPRs.bind(this);
-        this.getPRs(repo, user);
-        let prs = []
-        this.state= {
-            repo: repo,
-            user: user,
-            prs: prs,
-            activePR: prs && prs.length > 0 ? prs[0] : {id: null, clones: []},
-        };
+  constructor() {
+    super();
+    this.state = {repo: null, user:null, prs: [], activePR:newPR()};
+    let url = window.location.href;
+    // determine if url is http(s)://something/something/something
+    if (!url.match('https?://[^/]+/pr/[^/]+/[^/]+$')) {
+      console.log("badly formed url:");
+      console.log(url);
+      return;
     }
+    if (url.lastIndexOf("/") === url.length -1) {
+      url = url.substring(0,-1);
+    }
+    let sep = url.lastIndexOf("/");
+    let repo = url.substring(sep + 1);
+    let user = url.substring(url.substring(0,sep).lastIndexOf("/")+1, sep);
+    this.getPRs = this.getPRs.bind(this);
+    this.getPRs(repo, user);
+    let prs = []
+    this.state= {
+      repo: repo,
+      user: user,
+      prs: prs,
+      activePR: prs && prs.length > 0 ? prs[0] : newPR(),
+    };
+  }
 
-    changeActivePR(pr) {
+  changeActivePR(pr) {
+    this.setState(
+        {   repo: this.state.repo,
+          user: this.state.user,
+          prs: this.state.prs,
+          activePR: pr,
+        }
+        );
+  }
+
+  getPRs(repo,user) {
+    let fetchurl = "/api/pr/" + user + "/" + repo;
+    let promise = fetch(fetchurl)
+      .then(function(response) {
+        if (!response.ok) {
+          console.log("response not ok fetching " + fetchurl);
+          return;
+        }
+        var contentType = response.headers.get("Content-Type");
+        if(contentType && contentType.indexOf("application/json") !== -1) {
+          return response.json();
+        } else {
+          console.log("response not json fetching " + fetchurl);
+        }
+      });
+    promise.then((function(json) {
+      let val = json
         this.setState(
             {   repo: this.state.repo,
-                user: this.state.user,
-                prs: this.state.prs,
-                activePR: pr,
-            }
-        );
-    }
+              user: this.state.user,
+              prs: val,
+              activePR: val.length > 0 ? val[0] : {id: 0, clones: null},
+            })
+    }).bind(this))
+  }
 
-    getPRs(repo,user) {
-        let fetchurl = "/api/pr/" + user + "/" + repo;
-       let promise = fetch(fetchurl)
-            .then(function(response) {
-                if (!response.ok) {
-                    console.log("response not ok fetching " + fetchurl);
-                    return;
-                }
-                var contentType = response.headers.get("Content-Type");
-                if(contentType && contentType.indexOf("application/json") !== -1) {
-                    return response.json();
-                } else {
-                    console.log("response not json fetching " + fetchurl);
-                }
-            });
-       let action = function(json) {
-                let val = json
-                this.setState(
-                        {   repo: this.state.repo,
-                            user: this.state.user,
-                            prs: val,
-                            activePR: val.length > 0 ? val[0] : {id: 0, clones: null},
-                        })
-            }
-       action = action.bind(this)
-            promise.then(action)
-    }
-
-    render() {
-        console.log(this.state)
-        return (
-            <div className="App">
-                <div className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h2>Welcome to React</h2>
-                </div>
-                <header>
-                    <Head onSelect={this.changeActivePR} prs={this.state.prs} />
-                </header>
-                <nav className="clone-nav" >
-                    <Nav repo={this.state.repo} user={this.state.user} clones={this.state.activePR.clones} />
-                </nav>
-                <section className="clone-content" >
-                    <Display repo={this.repo} user={this.user} pr={this.activePR}/>
-                </section>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
+  render() {
+    console.log(this.state)
+      return (
+          <div className="App">
+            <div className="App-header">
+              <h2>Clone-browser</h2>
             </div>
-        );
-    }
+            <div className="App-content">
+              <header className="clone-head">
+                <Head   onSelect={this.changeActivePR} 
+                        prs={this.state.prs}
+                        repo={this.state.repo}
+                        user={this.state.user}/>
+              </header>
+              <nav className="clone-nav" >
+                <Nav
+                  clones={this.state.activePR.clones} />
+              </nav>
+              <section className="clone-content" >
+                <Display
+                  pr={this.state.activePR}/>
+              </section>
+              <div className="clearfix"></div>
+            </div>
+          </div>
+          );
+  }
 }
 
 export default App;
--- src/components.js
+++ src/components.js
@@ -1,52 +1,65 @@
-import React, { Componenet } from 'react';
+import React, { Component } from 'react';
 
 function Nav(props) {
-        let repo = props.repo;
-        let user = props.user;
-        let clones = props.clones;
+  let clones = props.clones;
+  return (
+      <div>
+      <h2>Clones</h2>
+      <ul>
+      {props.clones.map(c => {
         return (
-            <div>
-            <h2>Nav</h2>
-            <ul>
-                {props.clones.map(c => <li> {c.First.Filename} </li>)}
-            </ul>
-            </div>
-        );
+           <li> ({c.First.Filename}:{c.First.Byte},
+             <br />
+             {c.Second.Filename}:{c.Second.Byte})
+           </li>
+           )}
+          )}
+      </ul>
+      </div>
+      );
 }
 
 function Display(props) {
-        let repo = props.repo;
-        let user = props.user;
-        let clones = props.clones;
-        return (
-            <div>
-                <h2>Display</h2>
-                <div>
-                    <p>{repo}</p>
-                    <p>{user}</p>
-                    <p>{clones}</p>
-                </div>
-            </div>
-        );
+  let pr = props.pr ? props.pr : {files: [], path: ""};
+  console.log(pr);
+  return (
+      <div>
+        <h2>Display</h2>
+        <div className="col1">
+          <p>{pr.files.map(file => {
+            return (<span>
+                    Path: {file.path}
+                    <br />
+                    <pre><code>
+                        {window.atob(file.content)}
+                    </code></pre></span>
+            )})}
+          </p>
+        </div>
+        <div className="col2">
+        </div>
+      </div>
+      );
 }
 
 function Head(props) {
-    let onselect = function () {
-        var selection = document.GetElementById("prselect");
-        props.onSelect(selection.options[selection.selectedIndex].value);
-    }
+  let onselect = function () {
+    var selection = document.GetElementById("prselect");
+    props.onSelect(selection.options[selection.selectedIndex].value);
+  }
 
-    return (
-        <div>
-        <h2> Head </h2>
+  return (
+      <div>
+      <p>Repository: {props.user}/{props.repo}</p>
+        Pull Request (by ID):
         <select id="prselect" onChange={onselect}>
-            {props.prs.map(function(pr) {
-                return <option value={pr.id}> {pr.id} </option>
-            })}
+          {props.prs.map(function(pr) {
+            return <option key={pr.id} value={pr.id}> {pr.id} </option>
+                                  })}
         </select>
-        </div>
-    )
+      </div>
+      )
 }
- export {Nav};
- export {Display};
- export {Head};
+export {Nav};
+export {Display};
+export {Head};
